485444fcfe7550cf52728a3580cb813e92ecab72
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-06-05 18:54:51 -0300

    Adicionando primeiros componente e primeira view do nosso site

---
+++ front-end/minhas-financas-app/src/App.js
@@ -0,0 +1,13 @@
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+import Login from './views/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
