f9a9735d5703ba8265cecfeccaa67be7c2746a65
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-07-13 23:43:43 -0300

    Atualizando projeto

---
+++ front-end/minhas-financas-app/src/app/service/usuarioService.js
@@ -0,0 +1,21 @@
+import ApiService from "../apiservice";
+
+class UsuarioService extends ApiService{
+    constructor(){
+        super('/api/usuarios')
+    }
+
+    autenticar(credenciais){
+        return this.post('/autenticar',credenciais)
+    }
+
+    obterSaldoPorUsuario(id){
+        return this.get(`/${id}/saldo`)
+    }
+
+    salvar(usuario){
+        return this.post('/',usuario)
+    }
+}
+
+export default UsuarioService;
